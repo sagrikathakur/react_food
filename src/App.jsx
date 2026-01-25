746c1202914994407a61bb23b63882234e107129
sagrika thakur <sagrikathakur68@gmail.com>
2026-01-25 17:40:38 +0530

    react_food app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
