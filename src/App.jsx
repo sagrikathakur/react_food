0d7e6393ee1b3eaf91a4e6e1c53fca26c033ba98
sagrika thakur <sagrikathakur68@gmail.com>
2026-01-26 21:10:53 +0530

    added hero section  and food menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from './Components/navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Cart from './pages/home/Cart/Cart'
+import PlaceOrder from './pages/home/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+
+      {/* routes */}
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+
+      </Routes>
     </div>
   )
 }
