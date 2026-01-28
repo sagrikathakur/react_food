4e107f13747be50859d456e907e1e3b1cdc6cf14
sagrika thakur <sagrikathakur68@gmail.com>
2026-01-28 22:38:36 +0530

    added context api

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import { useState } from 'react'
 
 const Home = () => {
+
+  const [category, setCategory] = useState("All");
+
   return (
     <div>
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   )
 }
